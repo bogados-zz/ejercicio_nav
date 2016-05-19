06a3596aced86497ec0462a4b0cedaf125c35d4e
anonymus_sheep <seebogado@gmail.com>
2016-05-18 21:26:44 -0300

    Initial commit

---
+++ ejercicioNavent/src/main/java/ar/com/emanuel/bogado/ejercicioNavent/model/App.java
@@ -0,0 +1,13 @@
+package ar.com.emanuel.bogado.ejercicioNavent.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
